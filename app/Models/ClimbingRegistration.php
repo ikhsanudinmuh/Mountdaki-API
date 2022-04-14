f29de732735e169b508d1b9a1cb47544552cd32d
Muhammad Ikhsanudin <muhikhsanudin1@gmail.com>
2022-04-14 14:04:55 +0700

    add relation to Mountain model

--- app/Models/ClimbingRegistration.php
+++ app/Models/ClimbingRegistration.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ClimbingRegistration extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'mountain_id', 'user_id', 'identity_card', 'healthy_letter', 'schedule', 'status'
+    ];
+
+    public function mountain() {
+        return $this->belongsTo(Mountain::class);
+    }
 }
--- database/migrations/2022_04_14_062526_create_climbing_registrations_table.php
+++ database/migrations/2022_04_14_062526_create_climbing_registrations_table.php
@@ -14,7 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('climbing_registrations', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->integer('mountain_id');
+            $table->integer('user_id');
+            $table->text('identitiy_card');
+            $table->text('healthy_letter');
+            $table->date('schedule')->format('Y/m/d');
+            $table->enum('status', ['pending', 'approved', 'climbing', 'done'])->default('pending');
             $table->timestamps();
         });
     }
