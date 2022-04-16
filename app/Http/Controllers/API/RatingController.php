c15d1ccb3bb65b4957962a6986aaa93bd9c0c80f
Muhammad Ikhsanudin <muhikhsanudin1@gmail.com>
2022-04-16 21:42:09 +0700

    add function for store ratings

--- app/Http/Controllers/API/RatingController.php
+++ app/Http/Controllers/API/RatingController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\API\BaseController;
+use App\Models\ClimbingRegistration;
+use App\Models\Mountain;
+use App\Models\Rating;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
-class RatingController extends Controller
+class RatingController extends BaseController
 {
     /**
      * Display a listing of the resource.
@@ -14,7 +19,7 @@ class RatingController extends Controller
      */
     public function index()
     {
-        //
+        
     }
 
     /**
@@ -35,7 +40,32 @@ class RatingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $auth = $request->user();
+
+        $validator = Validator::make($request->all(), [
+            'climbing_registration_id' => 'required|numeric',
+            'review' => 'required',
+            'rate' => 'required'
+        ]);
+
+        
+        if ($validator->fails()) {
+            return $this->sendError($validator->errors(), [], 422);
+        }
+        
+        $mountain_id = ClimbingRegistration::where('id', $request->climbing_registration_id)->first()->mountain_id;
+        
+        $request->merge([
+            'mountain_id' => $mountain_id,
+            'user_id' => $auth->id
+        ]);
+
+        try {
+            $rating = Rating::create($request->all());
+            return $this->sendResponse($rating, 'Rating berhasil ditambahkan, terima kasih');
+        } catch (QueryException $e) {
+            return $this->sendError('Rating gagal ditambahkan', $e->errorInfo, 400);
+        }
     }
 
     /**
